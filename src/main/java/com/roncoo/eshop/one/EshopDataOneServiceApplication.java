10b758eb94daf0e6a9211fce1ad453197d6db95b
WUZe514 <18260624577@163.com>
2021-07-30 17:19:03 +0800

    dashboard问题修复

--- src/main/java/com/roncoo/eshop/one/EshopDataOneServiceApplication.java
+++ src/main/java/com/roncoo/eshop/one/EshopDataOneServiceApplication.java
@@ -1,11 +1,14 @@
 package com.roncoo.eshop.one;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,6 +19,16 @@ public class EshopDataOneServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EshopDataOneServiceApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
